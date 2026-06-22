25f8b7cd46acac0d24a4c87185ec00d560da5424
Zakariya <pzakariya@mail.ru>
2026-06-22 15:10:19 +0500

    fix capital letter in error message

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -19,14 +19,14 @@ type Posgtres struct {
 func New(cfg *config.Config, log *logger.Logger) (*Posgtres, error) {
 	pool, err := pgxpool.New(context.Background(), helper.GetDBDsn(cfg))
 	if err != nil {
-		log.Error("Failed to create pg pool", err)
+		log.Error("failed to create pg pool", err)
 		return nil, err
 	}
 	if err := pool.Ping(context.Background()); err != nil {
 		log.Error("failed to ping db", err)
 		return nil, err
 	}
-	log.Info("Database connected successfully")
+	log.Info("database connected successfully")
 	return &Posgtres{
 		log:  log,
 		Pool: pool,
